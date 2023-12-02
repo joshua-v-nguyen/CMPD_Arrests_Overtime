8a67fbd7f3d15207e8221c741519d546f6f8b372
joshua-v-nguyen <jvnguyen1234@gmail.com>
2023-12-01 23:34:46 -0500

    displaying 1 visual

--- App.py
+++ App.py
@@ -1,7 +1,16 @@
+import altair as alt
 import geopandas as gpd
 import pandas as pd
 import streamlit as st
 
+alt.data_transformers.disable_max_rows()
 cmpd_data = pd.read_csv('Data\Officer_Traffic_Stops.csv')
 
 st.title("Police Traffic Stops")
+
+chart = alt.Chart(cmpd_data).mark_bar().encode(
+    alt.X("Driver_Age:Q", bin=True),
+    y='count()',
+)
+
+st.altair_chart(chart)
