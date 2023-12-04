2324041ef55766218ddd2a347b4cc6a33971431b
joshua-v-nguyen <jvnguyen1234@gmail.com>
2023-12-04 18:30:37 -0500

    asdf

--- App.py
+++ App.py
@@ -7,7 +7,7 @@ alt.data_transformers.disable_max_rows()
 #importing CMPD data
 @st.cache_data
 def load_data(cmpd_data):
-    cmpd_data = pd.read_csv('C:\Users\Joshua\Desktop\Computer\DESKTOP\Visual Analytics\Streamlit app\Data\CMPD_Arrests.csv')
+    cmpd_data = pd.read_csv('Data\CMPD_Arrests.csv')
     return cmpd_data
 cmpd_data = load_data("<path to csv>")
 cmpd_data[['Year', 'Month']] = cmpd_data['Month_of_Stop'].str.split('/', expand=True)
