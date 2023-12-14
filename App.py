d62dd3e7c73f097ab956abf54a77676a8cefd40d
joshua-v-nguyen <jvnguyen1234@gmail.com>
2023-12-14 11:33:06 -0500

    Removed Population Tab

--- App.py
+++ App.py
@@ -130,121 +130,14 @@ def year_select(df,year_slider):
     df = df[df['Year']==year_slider]
     return df
 
-tab1, tab2, tab3 = st.tabs(["Background Information", "Population Graphs", "Proportional Arrests by Race"])
+tab1, tab2 = st.tabs(["Background Information", "Proportional Arrests by Race"])
 
 #==============POPULATION TAB==============#
 with tab1:
     st.write("Hello, I need information here lawl")
 
-#==============POPULATION TAB==============#
-with tab2:
-    if city == "FPD":
-        #Fayetteville, NC Arrest Data
-        fpd = year_select(fpd,slider)
-        cityname = "Fayetteville, NC"
-        pop = load_demo('Demographics.csv')
-        pop = pop[pop['City']=="FPD"]
-        @st.cache_data
-        def load_fpd_chart(df):
-            chart = alt.Chart(df).mark_bar(stroke='transparent').encode(
-                x=alt.X('Count:Q',title='',scale=alt.Scale(domain=[0, 100000])),
-                y= alt.Y('Race:N',title='').sort('-x'),
-                color = alt.Color('Race:N',legend=None,
-                                scale=alt.Scale(
-                                domain=df.sort_values(['Count'])['Race'].tolist(),
-                                range=['#003F5C','#58508D','#BC5090','#FF6361','#FFA600'])),
-                text=alt.Text('Count:Q',format=',.0f')
-            ).properties(
-                title=f"Population of {cityname} in {slider}",
-                width=600,
-                height=400
-            )
-            return chart
-        
-        chart = load_fpd_chart(pop)
-        text = chart.mark_text(align='left', dx=2,fontSize=15)
-
-        layer_chart= alt.layer(text,chart).configure_view(
-            stroke='transparent'
-        ).configure_axisY(
-            labelLimit=200,
-            labelFontSize=12,
-            labelColor='black'
-        )
-        st.altair_chart(layer_chart)
-
-    elif city =="LAPD":
-        #Los Angeles, CA Arrest Data
-        lapd = year_select(lapd,slider)
-        cityname = "Los Angeles, CA"
-        pop = load_demo('Demographics.csv')
-        pop = pop[pop['City']=="LAPD"]
-        @st.cache_data
-        def load_lapd_chart(df):
-            chart = alt.Chart(df).mark_bar(stroke='transparent').encode(
-                x=alt.X('Count:Q',title='',scale=alt.Scale(domain=[0, 2500000])),
-                y= alt.Y('Race:N',title='').sort('-x'),
-                color = alt.Color('Race:N',legend=None,
-                                scale=alt.Scale(
-                                domain=df.sort_values(['Count'])['Race'].tolist(),
-                                range=['#003F5C','#58508D','#BC5090','#FF6361','#FFA600'])),
-                text=alt.Text('Count:Q',format=',.0f')
-            ).properties(
-                title=f"Population of {cityname} in {slider}",
-                width=600,
-                height=400
-            )
-            return chart
-        
-        chart = load_lapd_chart(pop)
-        text = chart.mark_text(align='left', dx=2,fontSize=15)
-
-        layer_chart= alt.layer(text,chart).configure_view(
-            stroke='transparent'
-        ).configure_axisY(
-            labelLimit=200,
-            labelFontSize=12,
-            labelColor='black'
-        )
-        st.altair_chart(layer_chart)
-
-    elif city =="NOPD":
-        #New Orleans, LA Arrest Data
-        nola = year_select(nola,slider)
-        cityname = "New Orleans, LA"
-        pop = load_demo('Demographics.csv')
-        pop = pop[pop['City']=="NOPD"]
-        @st.cache_data
-        def load_nopd_chart(df):
-            chart = alt.Chart(df).mark_bar(stroke='transparent').encode(
-                x=alt.X('Count:Q',title='',scale=alt.Scale(domain=[0, 250000])),
-                y= alt.Y('Race:N',title='').sort('-x'),
-                color = alt.Color('Race:N',legend=None,
-                                scale=alt.Scale(
-                                domain=df.sort_values(['Count'])['Race'].tolist(),
-                                range=['#003F5C','#58508D','#BC5090','#FF6361','#FFA600'])),
-                text=alt.Text('Count:Q',format=',.0f')
-            ).properties(
-                title=f"Population of {cityname} in {slider}",
-                width=600,
-                height=400
-            )
-            return chart
-        
-        chart = load_nopd_chart(pop)
-        text = chart.mark_text(align='left', dx=2,fontSize=15)
-
-        layer_chart= alt.layer(text,chart).configure_view(
-            stroke='transparent'
-        ).configure_axisY(
-            labelLimit=200,
-            labelFontSize=12,
-            labelColor='black'
-        )
-        st.altair_chart(layer_chart)
-
 #==============ARREST TAB==============#
-with tab3:    
+with tab2:    
     st.write(f"**Arrests in :red[{city}]**")
 
     #LOADING DEMOGRAPHIC DATA
