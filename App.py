6b6b094b1384bc8f65604d71976887b9b0223cd6
joshua-v-nguyen <jvnguyen1234@gmail.com>
2023-12-14 23:18:15 -0500

    added white background to graph

--- App.py
+++ App.py
@@ -3,7 +3,7 @@ import pandas as pd
 import streamlit as st
 
 alt.data_transformers.disable_max_rows()
-alt.themes.enable('dark')
+alt.themes.enable('opaque')
 
 #==============IMPORTING DATA==============#
 
